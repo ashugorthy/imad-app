96c872c1b41d85c0913bc2ed805d31d85b6580bb
ashugorthy <ashugorthy@gmail.com>
2017-08-06 16:03:06 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,15 +10,15 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function(req,res){
-    res.sendFile('Testing article one');
+    res.send('Testing article one');
 });
 
 app.get('/article-two', function(req,res){
-    res.sendFile('Testing article two');
+    res.send('Testing article two');
 });
 
 app.get('/article-three', function(req,res){
-    res.sendFile('Testing article three');
+    res.send('Testing article three');
 });
 
 app.get('/ui/style.css', function (req, res) {
